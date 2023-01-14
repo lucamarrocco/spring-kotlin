faab9128dff10586126afb7a91cd273530f12579
lucamarrocco <43510+lucamarrocco@users.noreply.github.com>
2023-01-14 07:29:58 +0100

    Setting up simple KotlinJs for React

--- todo-react/src/main/kotlin/todo/control/TodoForm.kt
+++ todo-react/src/main/kotlin/todo/control/TodoForm.kt
@@ -1,4 +1,47 @@
 package todo.control
 
-class TodoForm {
+import org.w3c.dom.HTMLInputElement
+import react.FC
+import react.Props
+import react.dom.events.ChangeEventHandler
+import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.form
+import react.dom.html.ReactHTML.input
+import todo.model.Todo
+
+typealias ItemEventHandler<T> = (item: T) -> Unit
+typealias DismissEventHandler<T> = ItemEventHandler<T>
+typealias ConfirmEventHandler<T> = ItemEventHandler<T>
+
+
+external interface TodoFormProps : Props {
+    var item: Todo
+
+    var onDismiss: DismissEventHandler<Todo>
+    var onConfirm: ConfirmEventHandler<Todo>
+
+    var onChangeDescription: ChangeEventHandler<HTMLInputElement>
+}
+
+val TodoForm = FC<TodoFormProps> { props ->
+    form {
+        input {
+            autoFocus = true
+            name = "description"
+            value = props.item.description ?: ""
+            onChange = props.onChangeDescription
+        }
+
+        button {
+            +"Cancel"
+            onClick = { props.onDismiss(props.item) }
+        }
+
+        button {
+            +"Save"
+            onClick = { props.onConfirm(props.item) }
+        }
+
+        onSubmit = { event -> event.preventDefault(); props.onConfirm(props.item) }
+    }
 }
