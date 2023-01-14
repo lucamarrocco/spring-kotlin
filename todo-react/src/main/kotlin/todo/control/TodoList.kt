c0095a82460f427194ecaaae46bc30e52d827f47
lucamarrocco <43510+lucamarrocco@users.noreply.github.com>
2023-01-14 07:30:52 +0100

    Setting up simple KotlinJs for React

--- todo-react/src/main/kotlin/todo/control/TodoList.kt
+++ todo-react/src/main/kotlin/todo/control/TodoList.kt
@@ -1,33 +1,24 @@
-package domain.control
+package todo.control
 
-import domain.model.Todo
 import react.FC
 import react.Props
-import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.li
+import react.dom.html.ReactHTML.ul
+import todo.model.Todo
 
 external interface TodoListProps : Props {
     var items: List<Todo>
 }
 
-external interface TodoListItemProps : Props {
-    var item: Todo
-}
-
 val TodoList = FC<TodoListProps> { props ->
-    props.items
-        .map {
-            TodoListItem {
-                item = it
+    ul {
+        props.items
+            .map { todo ->
+                todo.description?.let {
+                    li {
+                        +it
+                    }
+                }
             }
-        }
-}
-
-val TodoListItem = FC<TodoListItemProps> { props ->
-    val item = props.item
-
-    val text = item.description ?: ""
-
-    div {
-        +text
     }
 }
